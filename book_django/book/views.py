3e117c29919168dc882ec38750fb65a11275d8a1
xu <xukang828@gmail.com>
2020-09-22 20:06:15 +0800

    项目初成

--- book_django/book/views.py
+++ book_django/book/views.py
@@ -1,3 +1,11 @@
+# 导入HttpResponse模块
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+# 定义视图函数
+def index(request):
+    return HttpResponse('ok')
