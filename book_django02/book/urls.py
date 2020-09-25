2c6d1d63901d53012e89cf02bba7b384f8c729bb
xu <xukang828@gmail.com>
2020-09-25 18:10:41 +0800

    转换器

--- book_django02/book/urls.py
+++ book_django02/book/urls.py
@@ -1,6 +1,29 @@
 from django.urls import path
-from book.views import goods
+from book.views import goods,content,method
+from django.urls import converters
+from django.urls.converters import register_converter
+# 1. 定义转换器
+class MobileConverter:
+    # 验证数据的关键是：　正则
+    regex = '1[3-9]\d{9}'
+
+    # 验证没有问题的数据，给视图函数
+    def to_python(self,value):
+        return value
+
+    # def to_url(self,values):
+    # # 将匹配结果用于反向解析传值是使用（了解）
+    # return value
+# 2.先注册转换器，才能在第三步中使用
+# converter 转换器类
+#　type_name 转换器的名字
+register_converter(MobileConverter,'phone')
+
 
 urlpatterns = [
-    path('<cat_id>/<goods_id>/',goods)
+    # <转换器名字：变量名>
+    # 转换器会对变量数据进行　正则验证
+    path('<int:cat_id>/<phone:goods_id>/',goods),
+    path('content/',content),
+    path('method/',method),
 ]
--- book_django02/book/views.py
+++ book_django02/book/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import json
 from django.shortcuts import render
 
 # Create your views here.
@@ -18,5 +19,34 @@ def goods(request,cat_id,goods_id):
 
 
     return HttpResponse("骑个饭店")
+
+def content(request):
+    dict_j = request.body
+    print(type(dict_j))  # bite类型需要转换成字符串
+    # print(dict_j)
+    dict_str = dict_j.decode()  # 字符串类型
+    # print(dict_str)
+    print(type(dict_str))
+   # dict_json = eval(dict_str)
+   #  print(dict_json)
+    dict_json = json.loads(dict_str)    # 使用json模块进行解码
+    print(dict_json)
+    print(type(dict_json))
+    # print(dict_json['name'])
+
+    ############请求头##############
+    # print(request.META)   # 获取请求头给的字典类型
+    # print(type(request.META))
+    # <class 'dict'>
+    return HttpResponse("ok")
+
+def method(request):
+    print(request.method)  # 返回请求的方式
+    return HttpResponse('method')
+
+from django.http import HttpResponse,JsonResponse
+def response(request):
+    return
+
 
 
